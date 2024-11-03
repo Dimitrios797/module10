11f409ed69f7b76f5cb571c3510a60d2fb3be492
Dimitrios797 <146129395+Dimitrios797@users.noreply.github.com>
2024-11-03 22:02:07 +0300

    Add files via upload

---
+++ module_10_3.py
@@ -0,0 +1,49 @@
+import threading
+import random
+import time
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            with self.lock:  # Блокируем доступ к балансу
+                self.balance += amount
+                print(f"Пополнение: {amount}. Баланс: {self.balance}")
+                if self.balance >= 500 and not self.lock.locked():
+                    self.lock.release()  # Разблокировка, если баланс >= 500
+            time.sleep(0.001)
+
+    def take(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            print(f"Запрос на {amount}")
+            with self.lock:  # Блокируем доступ к балансу
+                if amount <= self.balance:
+                    self.balance -= amount
+                    print(f"Снятие: {amount}. Баланс: {self.balance}")
+                else:
+                    print("Запрос отклонён, недостаточно средств")
+                    self.lock.acquire()  # Блокировка потока, если недостаточно средств
+            time.sleep(0.001)
+
+# Создание объекта класса Bank
+bk = Bank()
+
+# Создание потоков для методов deposit и take
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+# Запуск потоков
+th1.start()
+th2.start()
+
+# Ожидание завершения потоков
+th1.join()
+th2.join()
+
+# Вывод итогового баланса
+print(f'Итоговый баланс: {bk.balance}')
